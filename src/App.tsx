5e221fee34e5f063bc86ff647f1b01bf6d30cffe
Augustine Adjalogo-Agbeko <logos.agbeko@gmail.com>
2025-07-11 09:26:29 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div>
+      <p className="text-center font-bold text-xl underline">Hello</p>
+      <i className="ri-admin-line"></i>
+      <i className="ri-admin-fill"></i>
+    </div>
+  );
+}
